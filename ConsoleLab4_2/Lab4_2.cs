3fae3a3d34239f4635e8dc8ecd21c30df722249e
Роман Роман <romanbindyug@yandex.ru>
2022-01-09 20:30:58 +0300

    Готово

--- ConsoleLab4_2/Lab4_2.cs
+++ ConsoleLab4_2/Lab4_2.cs
@@ -46,31 +46,6 @@ namespace Lab4_1
                     Console.WriteLine("Чётных чисел {0} больше, чем нечётных чисел {1}", count2, count1);
                 }
             }
- 
-
-
-
-
-
-            /*
-            Console.WriteLine("Введите число");
-            Console.Write("N = ");
-            int N = Convert.ToInt32(Console.ReadLine());
-            int N2 = 0;
-            if (N > 0)
-            {
-                for (int i = 1; i <= N; i++)
-                {
-
-                    N2 = N2 + (2 * i - 1);
-                    Console.WriteLine(N2);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Введённое число должно быть >0");
-            }
-            */
             Console.Write("Для завершения нажмите любую клавишу");
             Console.ReadKey();
         }
